c1011f1c56e51f885e7559f007fcc491db4141b0
Dungta0603 <114870155+Dungta0603@users.noreply.github.com>
2022-11-02 16:53:56 +0700

    Revert "Revert "demo crucio""

---
+++ src/components/ComTam10.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="ComTam10">
+    <h1>{{ title1 }}</h1>
+    <hr style="background-color: #fff" class="we" />
+    <div class="ComTam10-nav">
+      <p>
+        {{ title2 }}
+      </p>
+      <p class="mini-text">{{ title3 }} -</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ComTam10 ",
+  data: function () {
+    return {
+      title1: "TESTIMONIALS",
+      title2:
+        "Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex eacommodi consequat. Quis aute iure reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur.",
+      title3: "- John K. ",
+    };
+  },
+};
+</script>
+
+<style scoped>
+.ComTam10 {
+  height: 500px;
+  background-color: #000000;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.ComTam10 h1 {
+  color: #fff;
+  margin: 25px;
+  font-weight: 500;
+  line-height: 1.2;
+  letter-spacing: 5px;
+}
+.ComTam10-nav {
+}
+.ComTam10-nav p {
+  margin: 30px;
+  width: 1000px;
+  color: #fff;
+  text-align: center;
+}
+.mini-text {
+  letter-spacing: 0;
+  line-height: 1;
+  font-size: 12px;
+}
+</style>
---
+++ src/components/ComTam6.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="ComTam6">
+    <div class="ComTam6-nav">
+      <h1>{{ tile1 }}</h1>
+      <div class="imfko">
+        <a href=""> {{ tile2 }}</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ComTam6",
+  data: function () {
+    return {
+      tile1: "WE CREATE AWESOME PROJECTS",
+      tile2: "GET STARTED",
+    };
+  },
+};
+</script>
+
+<style scoped>
+.ComTam6 {
+  height: 500px;
+}
+.ComTam6-nav {
+  background-color: var(--primary-color);
+  height: 360px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  color: #fff;
+}
+.ComTam6-nav h1 {
+  font-size: 40px;
+  font-weight: 100;
+  text-align: center;
+  letter-spacing: 10px;
+  line-height: 2;
+  margin-bottom: 30px;
+}
+.imfko {
+  border: 1px solid #fff;
+  width: 200x;
+  height: 50px;
+  display: flex;
+  align-items: center;
+}
+.imfko a {
+  color: var(--light-color);
+  text-decoration: none;
+  padding: 40px;
+  letter-spacing: 3px;
+}
+</style>
